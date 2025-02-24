12214f12b87842b97ac6c483c11cf1c80ccd1958
puranikvinit <kvp933.vinit@gmail.com>
2025-02-24 09:34:41 +0530

    [polybar] add polybar configs for asahi setup

---
+++ polybar/material/scripts/launcher.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/material/scripts/rofi/launcher.rasi
+rofi -c /home/kvp_933/.config/rofi/config.rasi -show drun -modi drun -font "JetBrains Mono 10"
